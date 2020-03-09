c254d622e8d33f91f990e63e44b8fa3ca68f748d
Cyrus Baker <cyrusbaker@czbaker232d.nmsu.edu>
2020-03-09 15:29:06 -0600

    updated landing page

--- landingPage.py
+++ landingPage.py
@@ -1,11 +1,21 @@
-from tkinter import *
-window=Tk()
-btn=Button(window, text="Submit", fg='blue')
-btn.place(x=80, y=100)
-lbl=Label(window, text="CS Helper", fg='red', font=("Helvetica", 16))
-lbl.place(x=60, y=50)
-txtfld=Entry(window, text="How May I Help You?", bd=5)
-txtfld.place(x=80, y=150)
-window.title('CS Helper')
-window.geometry("300x200+10+10")
-window.mainloop()
+import tkinter as tk
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 400, height = 300)
+canvas1.pack()
+
+entry1 = tk.Entry (root) 
+canvas1.create_window(200, 140, window=entry1)
+
+def getText ():  
+    x1 = entry1.get()
+    text = True
+    label1 = tk.Label(root, text= x1)
+    canvas1.create_window(200, 230, window=label1)
+    return text, x1
+
+button1 = tk.Button(text='Submit', command=getText)
+canvas1.create_window(200, 180, window=button1)
+
+root.mainloop()
