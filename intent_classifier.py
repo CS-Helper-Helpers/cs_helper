f4196dc5b21ffc70e80ee9cd4ae3457f455ca8b5
Cyrus Baker <cyrusbaker@czbaker232d.nmsu.edu>
2020-03-13 14:30:01 -0600

    IC

--- intent_classifier.py
+++ intent_classifier.py
@@ -29,29 +29,37 @@ from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, LSTM, Bidirectional, Embedding, Dropout
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-def load_dataset(filename):
+def load_dataset(piece = False):
     #df = pd.read_csv(filename, encoding = "latin1", names = ["Sentence", "Intent"])
-    engine = db.getBotDBEngine()
-    query = "select question as Sentence, cat as Intent from TrainingQuestions"
-    df = pd.read_sql_query(query, con = engine)
+    
+    if (not piece):
+        engine = db.getBotDBEngine()
+        query = "select question as Sentence, cat as Intent from TrainingQuestions"
+        df = pd.read_sql_query(query, con = engine)
 
-    print(df.head())
-    intent = df["Intent"]
-    unique_intent = list(set(intent))
-    sentences = list(df["Sentence"])
+        print(df.head())
+        intent = df["Intent"]
+        unique_intent = list(set(intent))
+        sentences = list(df["Sentence"])
 
-    query = "select count(*) from Categories"
-    df = pd.read_sql_query(query, con = engine)
-    catlength = df.loc[0].at["count(*)"]
-  
-    return (intent, unique_intent, sentences, catlength)
+        query = "select count(*) from Categories"
+        df = pd.read_sql_query(query, con = engine)
+        catlength = df.loc[0].at["count(*)"]
 
-intent, unique_intent, sentences, catlength = load_dataset("intents_and_categories.csv")
+        return (intent, unique_intent, sentences, catlength)
 
-nltk.download("stopwords")
-nltk.download("punkt")
+    else:
+        if (piece == "sentences"):
+            engine = db.getBotDBEngine()
+            query = "select question as Sentence from TrainingQuestions"
+            df = pd.read_sql_query(query, con = engine)
+            return list(df["Sentence"])
 
-stemmer = LancasterStemmer()
+        if (piece == "uniqueintents"):
+            engine = db.getBotDBEngine()
+            query = "select distinct cat as Intent from TrainingQuestions"
+            df = pd.read_sql_query(query, con = engine)
+            return list(df["Intent"])
 
 def cleaning(sentences):
     words = []
@@ -62,11 +70,7 @@ def cleaning(sentences):
         words.append([i.lower() for i in w])
     return words
 
-cleaned_words = cleaning(sentences)
-print(len(cleaned_words))
-print(cleaned_words[:3])
-
-def create_tokenizer( words, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'):
+def create_tokenizer( words, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'):
     """Create tokenizer
     """
     token = Tokenizer(filters=filters)
@@ -78,74 +82,93 @@ def get_max_length(words):
     """
     return(len(max(words, key=len)))
 
-
-word_tokenizer = create_tokenizer(cleaned_words)
-vocab_size = len(word_tokenizer.word_index) + 1
-max_length = get_max_length(cleaned_words)
-
-print("Vocab size = ", vocab_size, " and Maximum length = ", max_length)
-
 def encoding_doc(token, words):
     return(token.texts_to_sequences(words))
-    
-encoded_doc = encoding_doc(word_tokenizer, cleaned_words)
 
 def padding_doc(encoded_doc, max_length):
     return(pad_sequences(encoded_doc, maxlen=max_length, padding="post"))
 
-padded_doc = padding_doc(encoded_doc, max_length)
-padded_doc[:5]
-
-print("Shape of padded docs = ", padded_doc.shape)
-
-# tokenizer with filter changed
-output_tokenizer = create_tokenizer(unique_intent, filters='!"#$%&()*+,-/:;<=>?@[\]^`{|}~')
-                                    
-output_tokenizer.word_index
-
-encoded_output = encoding_doc(output_tokenizer, intent)
-encoded_output = np.array(encoded_output).reshape(len(encoded_output), 1)
-encoded_output.shape
-
 def one_hot(encode):
     o = OneHotEncoder(sparse=False)
     return(o.fit_transform(encode))
-    
-output_one_hot = one_hot(encoded_output)
-output_one_hot.shape
 
-from sklearn.model_selection import train_test_split
-train_X, val_X, train_Y, val_Y = train_test_split(padded_doc, output_one_hot, shuffle = True, test_size = 0.2)
-print("Shape of train_X = %s and train_Y = %s" % (train_X.shape, train_Y.shape))
-print("Shape of val_X = %s and val_Y = %s" % (val_X.shape, val_Y.shape))
-
-def create_model(vocab_size, max_length):
+def create_model(vocab_size, max_length, catlength):
     model = Sequential()
     model.add(Embedding(vocab_size, 128, input_length = max_length, trainable = False))
     model.add(Bidirectional(LSTM(128)))
-    # model.add(LSTM(128))
+#   model.add(LSTM(128))
     model.add(Dense(32, activation = "relu"))
     model.add(Dropout(0.5))
     model.add(Dense(catlength, activation = "softmax"))
-
+  
     return model
 
-model = create_model(vocab_size, max_length)
+def train_model():
 
-model.compile(loss = "categorical_crossentropy", optimizer = "adam", metrics = ["accuracy"])
-model.summary()
+    intent, unique_intent, sentences, catlength = load_dataset()
 
-filename = 'model.h5'
-checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=0, save_best_only=True, mode='min')
+    nltk.download("stopwords")
+    nltk.download("punkt")
 
-hist = model.fit(train_X, train_Y, epochs = 100, batch_size = 32, validation_data = (val_X, val_Y), callbacks = [checkpoint], verbose = 0)
+#    stemmer = LancasterStemmer()
+    LancasterStemmer()
 
-model = load_model("model.h5")
+    cleaned_words = cleaning(sentences)
+    print(len(cleaned_words))
+    print(cleaned_words[:3])
+    
+    word_tokenizer = create_tokenizer(cleaned_words)
+    vocab_size = len(word_tokenizer.word_index) + 1
+    max_length = get_max_length(cleaned_words)
 
-def predictions(text):
+    print("Vocab size = ", vocab_size, " and Maximum length = ", max_length)
+
+    encoded_doc = encoding_doc(word_tokenizer, cleaned_words)
+
+    padded_doc = padding_doc(encoded_doc, max_length)
+    padded_doc[:5]
+
+    print("Shape of padded docs = ", padded_doc.shape)
+
+    # tokenizer with filter changed
+    output_tokenizer = create_tokenizer(unique_intent, filters='!"#$%&()*+,-/:;<=>?@[\\]^`{|}~')
+                                    
+    output_tokenizer.word_index
+
+    encoded_output = encoding_doc(output_tokenizer, intent)
+    encoded_output = np.array(encoded_output).reshape(len(encoded_output), 1)
+    encoded_output.shape  
+
+    output_one_hot = one_hot(encoded_output)
+    output_one_hot.shape
+
+    from sklearn.model_selection import train_test_split
+    train_X, val_X, train_Y, val_Y = train_test_split(padded_doc, output_one_hot, shuffle = True, test_size = 0.2)
+    print("Shape of train_X = %s and train_Y = %s" % (train_X.shape, train_Y.shape))
+    print("Shape of val_X = %s and val_Y = %s" % (val_X.shape, val_Y.shape))
+
+    model = create_model(vocab_size, max_length, catlength)
+
+    model.compile(loss = "categorical_crossentropy", optimizer = "adam", metrics = ["accuracy"])
+    model.summary()
+
+    filename = 'model.h5'
+    checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=0, save_best_only=True, mode='min')
+
+    model.fit(train_X, train_Y, epochs = 100, batch_size = 32, validation_data = (val_X, val_Y), callbacks = [checkpoint], verbose = 0)
+#    hist = model.fit(train_X, train_Y, epochs = 100, batch_size = 32, validation_data = (val_X, val_Y), callbacks = [checkpoint], verbose = 0)
+
+def predictions(text, model):
     clean = re.sub(r'[^ a-z A-Z 0-9]', " ", text)
     test_word = word_tokenize(clean)
     test_word = [w.lower() for w in test_word]
+
+    #Remove this block if possible
+    sentences = load_dataset("sentences")
+    cleaned_words = cleaning(sentences)
+    max_length = get_max_length(cleaned_words)
+    word_tokenizer = create_tokenizer(cleaned_words)
+
     test_ls = word_tokenizer.texts_to_sequences(test_word)
     print(test_word)
     #Check for unknown words
@@ -157,7 +180,6 @@ def predictions(text):
     x = padding_doc(test_ls, max_length)
   
     pred = model.predict_proba(x)
-  
   
     return pred
 
@@ -181,13 +203,24 @@ def get_final_output(pred, classes):
                 print("%s has confidence = %s" % (classes[i], (predictions[i])))
     else:
         engine = db.getBotDBEngine()
-        print("The result is:")
-        print("%s with confidence = %s" % (classes[0], (predictions[0])))
         query = "select outvar from OutputVariables where inid in (select inputid from InputVariables where incat = '" + classes[0] + "')"
         df = pd.read_sql_query(query, con = engine)
         if (df.size == 1):
+            print("The result is:")
+            print("%s with confidence = %s" % (classes[0], (predictions[0])))
             print(df.loc[0].at["outvar"])
+            return df.loc[0].at["outvar"]
+        elif (df.size > 1):
+            print("Unable to get unique result.")
+            print("%s with confidence = %s" % (classes[0], (predictions[0])))
+        else:
+            print("Unable to find an answer for this question.")
+            print("%s with confidence = %s" % (classes[0], (predictions[0])))
+            
+    return ""
 
-text = "When does the semester end?"
-pred = predictions(text)
-get_final_output(pred, unique_intent)                                  
+def answer(text):
+    model = load_model("model.h5")
+    pred = predictions(text, model)
+    unique_intent = load_dataset("uniqueintents")
+    get_final_output(pred, unique_intent)
