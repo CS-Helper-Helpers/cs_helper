b35e44e7b3ffcf76cfcade5a84f343b6032918bd
Cyrus Baker <cyrusbaker@Cyruss-MacBook-Pro.local>
2020-03-12 22:25:26 -0600

    checkout

--- Database/engine.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-import Database.info as info
-def getBotDBEngine():
-    uri = "mysql+pymysql://" + info.user + ":" + info.password + "@" + info.host + "/" + info.database
-    return create_engine(uri)
--- Speech/__init__.py
+++
@@ -1,4 +0,0 @@
-from .assistantSpeaks import assistant_speaks
-from .getAudio import get_audio
-from .listening import listening
-from .wakeWord import wake_word
