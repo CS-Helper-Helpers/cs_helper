98c241bf9daa9d8df1daa505dafdc3126c79debb
Cyrus Baker <cyrusbaker@Cyruss-MacBook-Pro.local>
2020-04-15 19:00:12 -0600

    intent classifier tests

--- intent_classifier/tests/intent_classifier_test.py
+++ intent_classifier/tests/intent_classifier_test.py
@@ -4,14 +4,14 @@ from intent_classifier import IntentClassifier
 # ic = IntentClassifier()
 # print(ic)
 # ic.clean_utterance([])
-#print(ic.cleaning(["Hello  !"]))
+# print(ic.cleaning(["Hello  !"]))
+
 
 class TestIntentClassifier(unittest.TestCase):
-
     def test_macro(self):
         ic = IntentClassifier()
         ans = ic.my_test_return()
-        self.assertEqual(ans, 5)
+        self.assertEqual(ans, "hello")
 
     # @classmethod
     # def setUpClass(self):
@@ -31,13 +31,11 @@ class TestIntentClassifier(unittest.TestCase):
     #             cleaned_sentences, expected_sentences
     #     )
 
-    # def test_cleaning_empty_set(self):
-    #     sentences = []
-    #     cleaned_sentences = IntentClassifier().cleaning(sentences)
-    #     expected_sentences = []
-    #     self.assertEqual(
-    #             cleaned_sentences, expected_sentences
-    #     )
+    def test_cleaning_empty_set(self):
+        sentences = []
+        cleaned_sentences = IntentClassifier().cleaning(sentences)
+        expected_sentences = []
+        self.assertEqual(cleaned_sentences, expected_sentences)
 
     # def test_cleaning_nonstring_entry(self):
     #     sentences = ["The next entry", 2, "is a number"]
@@ -45,40 +43,39 @@ class TestIntentClassifier(unittest.TestCase):
     #             TypeError, IntentClassifier().cleaning(sentences)
     #     )
 
-    def test_create_tokenizer(self):
-        pass
+    # def test_create_tokenizer(self):
+    #     pass
 
-    def test_get_max_length(self):
-        words = ["this", "is", "a", "list", "of", "words"]
-        expected_max = 4
-        ic = IntentClassifier()
-        given_max = ic.get_max_length(words)
-        self.assertEqual(given_max, expected_max)
+    # def test_get_max_length(self):
+    #     words = ["this", "is", "a", "list", "of", "words"]
+    #     expected_max = 4
+    #     ic = IntentClassifier()
+    #     given_max = ic.get_max_length(words)
+    #     self.assertEqual(given_max, expected_max)
 
+    #     pass
 
-        pass
+    # def test_encoding_doc(self):
+    #     pass
 
-    def test_encoding_doc(self):
-        pass
+    # def test_padding_doc(self):
+    #     pass
 
-    def test_padding_doc(self):
-        pass
+    # def test_one_hot(self):
+    #     pass
 
-    def test_one_hot(self):
-        pass
+    # def test_create_model(self):
+    #     pass
 
-    def test_create_model(self):
-        pass
+    # def train_model(self):
+    #     pass
 
-    def train_model(self):
-        pass
+    # def test_predictions(self):
+    #     pass
 
-    def test_predictions(self):
-        pass
+    # def test_tests(self):
+    #     self.assertEqual(2, 2, "Should be 2")
 
-    def test_tests(self):
-        self.assertEqual(2,2, "Should be 2")
 
 # #if __name__ == '__main__':
 # #    unnittest.main()
-
