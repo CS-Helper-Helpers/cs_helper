5acd489d4a4c1585e238cd297538b45f52e28271
kathrine.swe <kathrine.swe@gmail.com>
2020-04-26 13:28:55 -0600

    pushing first push of important date chunker

---
+++ slots/important_date_chunker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf8
+"""Example of training an additional entity type
+This script shows how to add a new entity type to an existing pretrained NER
+model. To keep the example short and simple, only four sentences are provided
+as examples. In practice, you'll need many more — a few hundred would be a
+good start. You will also likely need to mix in examples of other entity
+types, which might be obtained by running the entity recognizer over unlabelled
+sentences, and adding their annotations to the training set.
+The actual training is performed by looping over the examples, and calling
+`nlp.entity.update()`. The `update()` method steps through the words of the
+input. At each word, it makes a prediction. It then consults the annotations
+provided on the GoldParse instance, to see whether it was right. If it was
+wrong, it adjusts its weights so that the correct action will score higher
+next time.
+After training your model, you can save it to a directory. We recommend
+wrapping models as Python packages, for ease of deployment.
+For more details, see the documentation:
+* Training: https://spacy.io/usage/training
+* NER: https://spacy.io/usage/linguistic-features#named-entities
+Compatible with: spaCy v2.1.0+
+Last tested with: v2.1.0
+"""
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+from pathlib import Path
+import spacy
+from spacy.util import minibatch, compounding
+from important_date_data import get_important_dates_train_data, get_important_dates_labels
+
+important_dates = {}
+
+def get_substring_label_truple(intent, label_indicator, label):
+    start = intent.find(label_indicator)
+    end = start + len(label_indicator)
+    return (start, end, label)
