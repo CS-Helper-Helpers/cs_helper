c53a24ca5e11ef4a37bbf9a8ebaaf4d24d187547
Arianna Martinez <42559506+armartin12@users.noreply.github.com>
2020-04-30 15:19:22 -0600

    Update engine.py

--- database/engine.py
+++ database/engine.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-import database.info_sample as info
+import database.info as info
 def getBotDBEngine():
     uri = "mysql+pymysql://" + info.user + ":" + info.password + "@" + info.host + "/" + info.database
     return create_engine(uri)
