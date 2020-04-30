933a5b0379273661b87490233ed2e31f472053e1
Cyrus Baker <cyrusbaker@Cyruss-MacBook-Pro.local>
2020-04-29 19:26:48 -0600

    added dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,46 @@ setup(name='CS Helper',
           'gtts',
           'tensorflow',
           'nltk',
-      ],
+      ],from setuptools import setup
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name="CS Helper",
+    version="0.1",
+    description="CS Helper, Here to Help",
+    url="https://github.com/CS-Helper-Helpers/cs_helper",
+    author="Team 3 AI Masters",
+    author_email="flyingcircus@example.com",
+    license="NA",
+    packages=[
+        "cs_helper",
+        "database",
+        "gui",
+        "input",
+        "intent_classifier",
+        "phone",
+        "slots",
+    ],
+    install_requires=[
+        "gtts",
+        "tensorflow",
+        "nltk",
+        "playsound",
+        "speechrecognition",
+        "SQLAlchemy",
+        "PyAudio",
+        "pymysql",
+    ],
+    zip_safe=False,
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
+
       zip_safe=False,
 
       test_suite='nose.collector',
