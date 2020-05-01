bc1e4884a928abe495b323d4f17e610b0a61562e
kathrine.swe <kathrine.swe@gmail.com>
2020-04-30 18:43:31 -0600

    We finding chunks, booiiii

--- slots/find_chunks.py
+++ slots/find_chunks.py
@@ -1,5 +1,23 @@
-def chunk_important_date():
-    """ find the chunks associated with an important date utterance """
+import os
+import spacy
 
-    pass
+def chunk_important_date(utterance):
+    """ find the chunks associated with an important date utterance """
+    # load model
+    
+    important_date_dir = os.path.join(os.getcwd(), 'important_date/')
+    id_nlp = spacy.load(important_date_dir)
+    #print(id_nlp.meta)
+    
+    doc = id_nlp(utterance)
+
+
+    print("Entities in '%s'" % utterance)
+    for ent in doc.ents:
+        print(ent.label_, ent.text)
+    # pass utterance through it
+    # return chunks
+    # pass
+
+chunk_important_date("When is spring break")
 
