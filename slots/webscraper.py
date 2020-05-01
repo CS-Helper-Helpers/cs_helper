d5920b9f8a54d2172f935b59ef9fc757a9ad5bd7
Cyrus Baker <cyrusbaker@Cyruss-MacBook-Pro.local>
2020-04-30 21:35:41 -0600

    webscraper working for courses and professor names

--- slots/webscraper.py
+++ slots/webscraper.py
@@ -1,20 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
+# COURSE CATALOG
+# URL = "https://catalogs.nmsu.edu/nmsu/arts-sciences/computer-science/#coursestext"
 
-URL = "https://catalogs.nmsu.edu/nmsu/arts-sciences/computer-science/#coursestext"
+# PROFESSOR PAGE
+URL = "https://www.cs.nmsu.edu/wp/people/faculty/"
+
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="coursestextcontainer")
-print(results.prettify())
+results = soup.find(id="awsm-team-5096")
+# print(results.prettify())
+
+# COURSES
 # job_elems = results.find_all("p", class_="courseblocktitle noindent")
-# CS_COURSE = []
 # for job_elem in job_elems:
 #     course_elem = job_elem.find("strong")
 #     course_info = str(course_elem.text)
 #     course_info = course_info.split(".")
 #     course_num = course_info[0]
 #     course_title = course_info[1]
-#     CS_COURSE.append(course_num)
-#     CS_COURSE.append(course_title)
-# print(CS_COURSE)
+#     # print(course_num)
+#     print(course_title)
+
+# PROF NAMES
+job_elems = results.find_all("div", class_="awsm-personal-info")
+for job_elem in job_elems:
+    prof_names = job_elem.find("h3")
+    print(prof_names.text)
